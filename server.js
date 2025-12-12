2e58150f2ace7ef409888c6d971c3efc9854b7ea
Rayne Tassone-Eckersall <tassonra@sheridancollege.ca>
2025-12-12 08:02:33 +0000

    fix

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const searchRoutes = require('./routes/searchRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 const externalBookRoutes = require('./routes/externalBookRoutes'); // ⬅ add this
 
-
-
 const app = express();
-
-const path = require("path");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
